004129c9c803afc90be14de09add39db7d340bc2
66010914132-hash <66010914132@msu.ac.th>
2026-02-17 19:21:33 +0700

    Add files via upload

--- a -1.php
+++ a -1.php
@@ -2,11 +2,36 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>กิตติ มีภาวงษ์(ฟิล์ม)</title>
+<title>66010914132 กิตติ มีภาวงษ์ (ฟิล์ม)</title>
 </head>
 
 <body>
+<h1>งาน i --66010914132 กิตติ มีภาวงษ์ (ฟิล์ม)</h1>
 
-<h1>งาน i -- กิตติ มีภาวงษ์(ฟิล์ม)</h1>
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM regions";
+$rs = mysqli_query($conn, $sql);
+?>
+
+<table border="1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+    </tr>
+<?php
+while ($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['r_id']; ?></td>       
+        <td><?php echo $data['r_name']; ?></td>
+        <td width="80" align="center"><img src="../imges/ดาวน์โหลด.jpg" width="20"></td>
+    </tr>
+<?php
+}
+mysqli_close($conn);
+?>
+</table>
+
 </body>
 </html>
